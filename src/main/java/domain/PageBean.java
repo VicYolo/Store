d50c6b8c8813a84d49d2ee0be11deea38b404563
Aristotle <1981305768@qq.com>
2022-06-22 10:25:11 +0800

    modify the html

--- src/main/java/domain/PageBean.java
+++ src/main/java/domain/PageBean.java
@@ -2,30 +2,26 @@ package domain;
 
 import java.util.List;
 
+/**
+ * 分页实体
+ * @author Administrator
+ * @param <T>
+ *
+ */
 public class PageBean<T> {
-	private List<T> data;//当前页的数据		查询  limit (pageNumber-1)*pageSize,pageSize
-	private int pageNumber;//当前页			页面传递过来
-	private int totalRecord;//总条数		查询  count(*)
-	private int pageSize;//每页显示的数量	固定值
-	private int totalPage;//总页数			计算出来 (int)Math.ceil(totalRecord*1.0/pageSize);
+	private int pageNumber;//当前页面
+	private int pageSize;//每页显示的条数
+	private int totalRecord;//总条数 查询出来
+	private int totalPage;//总页数	计算
+	private List<T> data;//当前页的数据 查询 
+	private int startIndex;//limit后面的m (pageNumber-1)*pageSize
 	
-	public List<T> getData() {
-		return data;
-	}
-	public void setData(List<T> data) {
-		this.data = data;
-	}
+	
 	public int getPageNumber() {
 		return pageNumber;
 	}
 	public void setPageNumber(int pageNumber) {
 		this.pageNumber = pageNumber;
-	}
-	public int getTotalRecord() {
-		return totalRecord;
-	}
-	public void setTotalRecord(int totalRecord) {
-		this.totalRecord = totalRecord;
 	}
 	public int getPageSize() {
 		return pageSize;
@@ -33,27 +29,44 @@ public class PageBean<T> {
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
+	public int getTotalRecord() {
+		return totalRecord;
+	}
+	
+	public void setTotalRecord(int totalRecord) {
+		this.totalRecord = totalRecord;
+	}
+	
 	/**
 	 * 获取总页数
 	 * @return
 	 */
 	public int getTotalPage() {
-		return (int)Math.ceil(totalRecord*1.0/pageSize);
+		return (int) Math.ceil(this.totalRecord*1.0/pageSize);
 	}
 	
-	/**
-	 * 获取开始索引
-	 * @return
-	 */
-	public int getStartIndex(){
-		return (pageNumber-1)*pageSize;
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+	public List<T> getData() {
+		return data;
+	}
+	public void setData(List<T> data) {
+		this.data = data;
 	}
 	
+	public int getStartIndex() {
+		return (this.pageNumber-1)*this.pageSize;
+	}
+	
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+	public PageBean() { }
+
 	public PageBean(int pageNumber, int pageSize) {
-		super();
 		this.pageNumber = pageNumber;
 		this.pageSize = pageSize;
 	}
-	
-	
+
 }
